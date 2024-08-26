b3b46c3b9269674a83cf29bd14ff98e0edcfcad4
Kudsun Khamjamnun <kudsun.k@gmail.com>
2024-08-26 15:53:10 +0700

    change get all to get me

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const authMiddleware = require('../middleware/auth');
-
+const jwt = require('jsonwebtoken');
 
 /**
  * @swagger
@@ -67,9 +69,9 @@ router.post('/create', async (req, res) => {
 
 /**
  * @swagger
- * /users/get:
+ * /users/get/me:
  *   get:
- *     summary: Get all users
+ *     summary: Get user by accessToken
  *     tags: [User]
  *     responses:
  *       200:
@@ -77,18 +79,64 @@ router.post('/create', async (req, res) => {
  *         content:
  *           application/json:
  *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/User'
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                 displayName:
+ *                   type: string
+ *                 username:
+ *                   type: string
+ *                 password:
+ *                   type: string
+ *       401:
+ *         description: Unauthorized
  *       500:
  *         description: Server error
  */
-router.get('/get',authMiddleware, async (req, res) => {
+router.get('/get/me',authMiddleware, async (req, res) => {
     try {
-        const users = await User.find();
-        res.json(users);
+        const authHeader = req.headers['authorization'];
+        const token = authHeader.split(' ')[1];
+        if (!token) {
+            return res.sendStatus(401);
+        }
+        const accessTokenSecret = process.env.ACCESS_TOKEN_KEY;
+        jwt.verify(token, accessTokenSecret, async (err, user) => {
+            if (err) {
+                return res.sendStatus(401);
+            }
+
+            const users = await User.findOne({ _id: user.userId });
+            if(!users) {
+                return res.status(400).json({ message: 'User not found' });
+            }
+            res.json(users);
+        });
     } catch (err) {
         res.status(500).json({ message: err.message });
+    }
+});
+
+
+router.patch('/update/me', authMiddleware, async (req, res) => {
+    try {
+        const authHeader = req.headers['authorization'];
+        const token = authHeader.split(' ')[1];
+        const user = await User.findById(req.params.id);
+        if (req.body.displayName) {
+            user.displayName = req.body.displayName;
+        }
+        if (req.body.username) {
+            user.username = req.body.username;
+        }
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+        const updatedUser = await user.save();
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
     }
 });
 
